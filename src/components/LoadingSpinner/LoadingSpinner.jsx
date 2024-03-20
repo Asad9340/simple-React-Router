3f104ce5acfb8cbb584af93f67d3f5422a6a976c
Asad9340 <asadulimran1999@gmail.com>
2024-03-21 01:58:34 +0600

    added extra margin bottom on loder

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,7 +1,7 @@
 import { BounceLoader } from "react-spinners";
 function LoadingSpinner() {
   return (
-    <div className="flex justify-center items-center my-10 py-6">
+    <div className="flex justify-center items-center my-10 py-6 mb-10">
       <BounceLoader color="#36d7b7" />
     </div>
   );
