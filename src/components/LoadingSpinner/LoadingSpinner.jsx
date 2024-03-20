cf3d389c19f50f89465ef407e4b48ff3ab248bf0
Asad9340 <asadulimran1999@gmail.com>
2024-03-20 18:05:18 +0600

    added loader sipinner

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,7 +1,7 @@
 import { BounceLoader } from "react-spinners";
 function LoadingSpinner() {
   return (
-    <div className="flex justify-center items-center my-10 my-6">
+    <div className="flex justify-center items-center my-10 py-6">
       <BounceLoader color="#36d7b7" />
     </div>
   );
